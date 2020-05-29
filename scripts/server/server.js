d7df87e4ccc60f37ea2e111d54eba889453ad7e3
aries1579 <nastja_osipova@mail.ru>
2020-05-30 00:35:54 +0300

    Add files via upload

--- scripts/server/server.js
+++ scripts/server/server.js
@@ -5,25 +5,6 @@ var pollArray;
 
 // Setup which events to listen for
 serverSystem.initialize = function () {
-	// Register any events you will send to the client
-	// system.registerEventData(...)
-
-	// Register any components you will attach to game objects
-	// system.registerComponent(...)
-
-	// Set up any events you wish to listen to
-	serverSystem.listenForEvent("mymod3:pinky", eventData => receivePinkyMessage(eventData));
-
-
-	// Enable full logging, useful for seeing errors, you will probably want to disable this for
-	// release versions of your scripts.
-	// Generally speaking it's not recommended to use broadcastEvent in initialize, but for configuring logging it's fine.
-	const scriptLoggerConfig = serverSystem.createEventData("minecraft:script_logger_config");
-	scriptLoggerConfig.data.log_errors = true;
-	scriptLoggerConfig.data.log_information = true;
-	scriptLoggerConfig.data.log_warnings = true;
-	serverSystem.broadcastEvent("minecraft:script_logger_config", scriptLoggerConfig);
-
 	tickIndex = 0;
 	rainIsOn = false;
 	serverSystem.listenForEvent("minecraft:weather_changed", eventData => rainOn(eventData));
@@ -31,9 +12,7 @@ serverSystem.initialize = function () {
 	pollArray = [ [1, 4, -8], [-21, 4, 1] ];
 }
 
-// per-tick updates
 serverSystem.update = function() {
-	// Any logic that needs to happen every tick on the server.
 	tickIndex++;
 
 	if(tickIndex==100){
@@ -50,13 +29,6 @@ serverSystem.update = function() {
 		}
 		
 		///fill ~-3 ~-3 ~-3 ~3 ~-1 ~3 water
-	}
-}
-
-function receivePinkyMessage(parameters) {
-	if (parameters.data.narf) {
-		//set up chat event data object
-		consoleLog("Hello world");
 	}
 }
 
